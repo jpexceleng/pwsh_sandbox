57c065ad189061041364751e1e2838a928c249a2
Jed Peterson <jed.peterson@exceleng.net>
2023-06-13 07:43:15 -0500

    pwsh updates

--- 24_pnp_pwsh.ps1
+++ 24_pnp_pwsh.ps1
@@ -1,27 +1,27 @@
 # The following demonstrates how PnP.PowerShell module can be used to 
 # programmatically create a new SharePoint page with an embedded file.
 
-$SiteURL="https://exceng.sharepoint.com/sites/KB/"
+$SiteUrl="https://exceng.sharepoint.com/sites/KB/"
 $PageName="TestPage"
 
 try {
 
-# connect to sharepoint site
-Connect-PnPOnline -Url $SiteURL -Interactive
+    # connect to sharepoint site
+    Connect-PnPOnline -Url $SiteUrl -Interactive
    
-# create new test page
-# Add-PnPPage `
-#     -Name $PageName `
-#     -Title "This is a test page" `
-#     -LayoutType Article `
-#     -CommentsEnabled:$false
+    # create new test page
+    Add-PnPPage `
+        -Name $PageName `
+        -Title "Mounds View Office Map" `
+        -LayoutType Article `
+        -CommentsEnabled:$false
 
-# add new section to test page
-# Add-PnPPageSection `
-#     -Page $PageName `
-#     -SectionTemplate OneColumn
+    # add new section to test page
+    Add-PnPPageSection `
+        -Page $PageName `
+        -SectionTemplate OneColumn
 
-# add new page web part to embed a document page section
+    # add new page web part to embed a document page section
     Write-Output "Adding web part..."
     Add-PnPPageWebPart `
         -Page $PageName `
@@ -29,32 +29,37 @@ Connect-PnPOnline -Url $SiteURL -Interactive
         -Section 1 `
         -Column 1
 
-# Get page web part
+    # Get page web part
     Write-Output "Getting web part..."
     $Page = Get-PnPPage -Identity $PageName
     $WebPart = Get-PnPPageComponent -Page $Page
 
-# The following will output the JSON properties of the newly created web part
-# $Test = Get-PnPPageComponent $PageName
-# $Test.PropertiesJson | ConvertFrom-Json | ConvertTo-Json
+    # The following will output the JSON properties of the newly created web part
+    # $Test = Get-PnPPageComponent $PageName
+    # $Test.PropertiesJson | ConvertFrom-Json | ConvertTo-Json
 
-# modify page web part properties
+    # modify page web part properties
     $PropertiesJson = Get-Content "res/propertiesjson.json"
     $PropertiesJson = [string]$PropertiesJson
     Write-Output "Setting web part properties..."
     Set-PnPPageWebPart -Page $Page -Identity $WebPart.InstanceId -PropertiesJson $PropertiesJson
 
-# remove page web part
-# Remove-PnPPageComponent -Page $Page -InstanceId $WebPart.InstanceId
+    # delete web part from page
+    # Remove-PnPPageComponent -Page $Page -InstanceId $WebPart.InstanceId
+    
+    # delete page from sharepoint
+    # Remove-PnPPage -Identity $PageName
 
-# publish page
+    # publish page
     Write-Output "Publishing..."
-    $Page.Publish()
-
-# disconnect from sharepoint site
-    Write-Output "Disconnecting from SharePoint..."
-    Disconnect-PnPOnline
+    $Page = Get-PnPPage -Identity $PageName
+    $Page.Publish("Published")
 }
 catch {
-    Write-Error "Something went wrong! $($_.Exception.Message)"
+    Write-Error "$($_.Exception.Message)"
+}
+finally {
+    # disconnect from sharepoint site
+    Write-Output "Disconnecting from SharePoint..."
+    Disconnect-PnPOnline
 }
